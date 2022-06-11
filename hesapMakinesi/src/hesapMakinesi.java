f7af17e761045fd44d516d5930e3811129f8667c
Atayazz <atayaz99@hotmail.com>
2022-06-11 15:47:00 +0300

    a

--- hesapMakinesi/src/hesapMakinesi.java
+++ hesapMakinesi/src/hesapMakinesi.java
@@ -3,11 +3,23 @@
 
         public static void main(String[] args) {
 
-        
+            int a , b, c;
+            a = 10;
+            b = 15;
+            c = 5;
 
-
-
-
+           /* boolean kosul1 = (a==b);
+            boolean kosul2 = (a<=b);
+            boolean kosul3 = (a + c == b);
+            boolean sonuc1 = (a==b || a<=b);
+            System.out.println("sonucunuz dogrudur." kosul3);
+        */
+        if  {
+            System.out.println("Sonuc dogrudur.");
+        }
+        else {
+            System.out.println("Sonuc yanlistir.");
+            };
 
 
     }
