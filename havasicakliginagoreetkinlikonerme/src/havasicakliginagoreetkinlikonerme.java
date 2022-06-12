c20654623c7cbaeb430123a0e349851d37674352
Atayazz <atayaz99@hotmail.com>
2022-06-12 16:12:44 +0300

    last

--- havasicakliginagoreetkinlikonerme/src/havasicakliginagoreetkinlikonerme.java
+++ havasicakliginagoreetkinlikonerme/src/havasicakliginagoreetkinlikonerme.java
@@ -4,6 +4,20 @@ public class havasicakliginagoreetkinlikonerme {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        double x;
+        System.out.print("Lutfen hava sicakligini giriniz: ");
+        x = input.nextDouble();
+        if (x < 5) {
+            System.out.println("Kayak yapabilirsiniz.");
+        } else if ((10 <= x) && (x <= 15)) {
+            System.out.println("Hem sinema hem de piknik yapabilirsiniz.");
+        } else if ((5 <= x) && (x < 15)) {
+            System.out.println("Sinema yapabilirsiniz.");
+        } else if ((10 < x) && (x < 25)) {
+            System.out.println("Piknik yapabilirsiniz.");
+        } else if (x > 25) {
+            System.out.println("Yuzme yapabilirsiniz.");
+        }
 
 
     }
