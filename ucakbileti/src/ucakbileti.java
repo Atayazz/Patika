db598541ef51f1262219d1ab4d877df1131c9b5c
Atayazz <atayaz99@hotmail.com>
2022-06-13 02:06:49 +0300

    a

---
+++ ucakbileti/src/ucakbileti.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ucakbileti {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a;
+        a = input.nextInt();
+
+    }
+}
