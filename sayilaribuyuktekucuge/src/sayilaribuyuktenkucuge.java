56a432ea6b8dcca8da11836b92f992b0d9198fb5
Atayazz <atayaz99@hotmail.com>
2022-06-12 16:27:05 +0300

    last

---
+++ sayilaribuyuktekucuge/src/sayilaribuyuktenkucuge.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class sayilaribuyuktenkucuge {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("1. Sayiyi giriniz.");
+        a = input.nextInt();
+        System.out.println("2. Sayiyi giriniz.");
+        b = input.nextInt();
+        System.out.println("3. Sayiyi giriniz.");
+        c = input.nextInt();
+
+        if (a > b && a > c)
+            if (b > c)
+                System.out.println(a + " >" + b + " >" + c);
+            else System.out.println(a + " >" + c + " >" + b);
+
+        if (b > a && b > c)
+            if (a > c)
+                System.out.println(b + " >" + a + " >" + c);
+            else System.out.println(b + " >" + c + " >" + a);
+
+        if (c > a && c > b)
+            if (a > b)
+                System.out.println(c + " >" + a + " >" + b);
+            else System.out.println(c + " >" + b + " >" + a);
+
+
+    }
+}
