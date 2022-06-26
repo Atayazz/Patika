45cddf7e9762117fb9ef862dc10bb1b1242b99e2
Atayazz <atayaz99@hotmail.com>
2022-06-26 22:56:58 +0300

    a

---
+++ girilensayiyakadarciftsayilaribulanprog/src/sayicift.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sayicift {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int k;
+        System.out.println("Bir sayi giriniz:");
+        k = input.nextInt();
+
+        for (int i = 1; i <= k; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
