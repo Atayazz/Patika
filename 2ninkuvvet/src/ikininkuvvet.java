fa7a60d87476aa241c8763f033a13fbf00037070
Atayazz <atayaz99@hotmail.com>
2022-07-10 15:45:22 +0300

    a

--- 2ninkuvvet/src/ikininkuvvet.java
+++ 2ninkuvvet/src/ikininkuvvet.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class ikininkuvvet {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-
- 
+        System.out.println("Lutfen bir sayi giriniz:    ");
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i < 5);
+        
     }
 }
+
 
 
 
